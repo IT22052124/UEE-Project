a6ae2dc17320576f9b5d0e4fd274f87d75a6a555
Mohamed Mufeez <129639317+IT22320896@users.noreply.github.com>
2024-10-09 23:50:40 +0530

    hi

--- Backend/Routes/JobRoute.js
+++ Backend/Routes/JobRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllJobs, applyJob } from "../Controllers/JobController.js"; // Import your user controller
+import { getAllJobs, applyJob, checkJob } from "../Controllers/JobController.js"; // Import your user controller
 
 const router = express.Router();
 
 router.get("/Jobs", getAllJobs);
 router.post("/apply", applyJob);
+router.get("/check", checkJob);
 
 export default router;
